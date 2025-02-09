62aeda39be4269f1a4a95ea6c05bb9faf1541461
utkarshkalra <utkarshkalra.2001@gmail.com>
2025-02-09 19:28:15 +0530

    fixing image issue

--- client/src/component/Map/Map.jsx
+++ client/src/component/Map/Map.jsx
@@ -2,8 +2,16 @@ import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import { isMobile } from "../../Utils.js";
 import PropTypes from "prop-types";
 import CustomPopup from "./Popup.jsx";
-
+import L from "leaflet";
 const Map = ({ clusters }) => {
+  const customIcon = L.icon({
+    iconUrl: "../../../src/assets/marker-icon-2x-blue.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: "../../../src/assets/marker-shadow.png",
+    shadowSize: [41, 41],
+  });
   return (
     <div className="w-full md:w-[75%] h-[370px]">
       <MapContainer
@@ -21,6 +29,7 @@ const Map = ({ clusters }) => {
           <Marker
             key={cluster.id}
             position={[cluster.location.latitude, cluster.location.longitude]}
+            icon={customIcon}
           >
             <CustomPopup cluster={cluster} />
           </Marker>
