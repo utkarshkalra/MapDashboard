18d3c8ba2e5e230992335cd83d92ccdd88e13202
utkarshkalra <utkarshkalra.2001@gmail.com>
2025-02-09 19:32:45 +0530

    marker image fix

--- client/src/component/Map/Map.jsx
+++ client/src/component/Map/Map.jsx
@@ -5,11 +5,13 @@ import CustomPopup from "./Popup.jsx";
 import L from "leaflet";
 const Map = ({ clusters }) => {
   const customIcon = L.icon({
-    iconUrl: "../../../src/assets/marker-icon-2x-blue.png",
+    iconUrl:
+      "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png",
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
-    shadowUrl: "../../../src/assets/marker-shadow.png",
+    shadowUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
     shadowSize: [41, 41],
   });
   return (
